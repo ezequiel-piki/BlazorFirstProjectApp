5fbdb4694b1a5a4a8545d6a4ffdd59477a366d5c
ezequ <ezequ@EZEQUIEL>
2023-03-09 23:43:03 -0300

    Agregar archivos de proyecto.

---
+++ BlazorExpenseTracker.Model/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpenseTracker.Model
+{
+    public class Category
+    {
+       // [Range(1,10)] el formulario va ser invalido si se pasa de este rango 1-10
+       public int Id { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Category Name is required")]
+        // [EmailAddress] el campo deberá ser de tipo email
+        //[StringLength(25)]
+        [MinLength(2)]
+        public string Name { get; set; }
+    }
+}
